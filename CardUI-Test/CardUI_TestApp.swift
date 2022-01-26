a977349763fd6d510fe2ca452fe6dd90753bd9c8
bahrihrf34 <baybarskan34@gmail.com>
2022-01-26 23:10:10 +0300

    Initial Commit

---
+++ CardUI-Test/CardUI_TestApp.swift
@@ -0,0 +1,17 @@
+//
+//  CardUI_TestApp.swift
+//  CardUI-Test
+//
+//  Created by Admin on 26.01.2022.
+//
+
+import SwiftUI
+
+@main
+struct CardUI_TestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
